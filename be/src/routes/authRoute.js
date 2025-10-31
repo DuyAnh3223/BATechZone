88b619e5f274116283bbaafc7d72041ec1e21768
Duy Anh <duyanh3203@gmail.com>
2025-11-01 01:04:25 +0700

    feat(be): init be structure

---
+++ be/src/routes/authRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { signOut,signIn, signUp } from '../controllers/authController.js';
+
+const router = express.Router();
+
+router.post("/signup",signUp);
+router.post("/signin",signIn);
+router.post("/signout",signOut);
+
+
+export default router;
---
+++ be/src/server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { testConnection } from './libs/db.js';
+import authRoute from './routes/authRoute.js';
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middlewares
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get('/api/test', (req, res) => {
+    res.json({ 
+        success: true,
+        message: 'Welcome to PC Hardware Store API',
+        timestamp: new Date(),
+        version: '1.0.0'
+    });
+});
+
+//public routes
+app.use('/api/auth', authRoute);
+
+// Handle 404
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: `Cannot ${req.method} ${req.url}`
+    });
+});
+
+// Database connection and server start
+const startServer = async () => {
+  try {
+    await testConnection();
+    app.listen(PORT, () => {
+      console.log(`✅ Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
